e0ab1f0d3d46179ad90d7ba4e742e282e938ecbe
Dylan <dylanw@uci.edu>
2020-12-01 12:14:50 -0800

    added scroll up

--- script.js
+++ script.js
@@ -6,7 +6,20 @@ $(document).ready(function(){
         else{
             $('.navbar').removeClass("sticky")
         }
-    })
+        // scroll-up button show/hide script
+        if(this.scrollY > 500){
+            $('.scroll-up-btn').addClass("show");
+        }else{
+            $('.scroll-up-btn').removeClass("show");
+        }
+    });
+
+    // slide-up script
+    $('.scroll-up-btn').click(function(){
+        $('html').animate({scrollTop: 0});
+        // removing smooth scroll on slide-up button click
+        $('html').css("scrollBehavior", "auto");
+    });
     // toggle menu/navbar script
     $('.menu-btn').click(function(){
         $('.navbar .menu').toggleClass("active")
